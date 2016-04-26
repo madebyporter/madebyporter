068478db68cf0ae712f988b73ea9e54236c42a99
Chris Porter <chrisporter@nowthatsgenius.com>
2016-04-26 01:38:54 -0700

    worked on play/pause state

--- assets/js/raw/global.js
+++ assets/js/raw/global.js
@@ -37,52 +37,56 @@ js.main = {
     var api = '71a3575c851dae984aa4222250977ad1';
     var redirect = 'http://localhost:8888/mbp/madebyporter/_build/music-higher.html';
 
-    // $.get('http://api.soundcloud.com/resolve.json?url=https://soundcloud.com/madebyporter/sets/a-story-to-tell-freeway&client_id=' + api, function (result) {
-    //   console.log(result);
-    // });
-
     SC.initialize({
       client_id: api,
       redirect_uri: redirect,
     });
 
     var track_objects = {};
-
-    // SC.get('/users/' + USER + '/playlists', function(playlists) {
     SC.get('/playlists/31545797').then(function(playlists){
       console.log(playlists.title);
-      var sound;
       $(playlists).each(function(index, playlist) {
         var $playlist = $('.mbp-player-tracks-list').eq(index);
-
-        // $playlist.append($('<li class="track"></li>').html('<div class="name">' + track.title + '</div>'));
         var $tracks = $playlist.find('.mbp-player-tracks-list-names-inner');
         $(playlist.tracks).each(function(i, track) {
-          $tracks.append($('<li data-id="' + track.id + '" data-name="' + track.title + '" class="track"></li>').html('<div class="name">' + track.title + '</div><div class="controls"><div class="controls-ele controls-seek">Seek</div><div class="controls-ele controls-info">Info</div>'));
+          $tracks.append($('<li data-id="' + track.id + '" data-name="' + track.title + '" class="track"></li>').html('<div class="name">' + track.title + '</div><div class="controls"><div class="controls-ele controls-seek">Seek</div><div class="controls-ele controls-state"><div class="controls-state-playing">Playing</div><div class="controls-state-paused">Paused</div></div>'));
         });
 
         var $track = $tracks.find('.track');
-        
+        var sound;
         $track.on('tap',function(){
           var track_id = $(this).attr('data-id');
           var track_name = $(this).attr('data-name');
           console.log(track_id);
-
-          SC.stream('/tracks/' + track_id).then(function(sound){
-            sound.play();
-            sound.on("state-change", function(state) {
-              console.log(state);
+          currentSound = sound;
+          if( sound ) {
+            if(is_playing) {
+                currentSound.pause();
+                is_playing = false;
+                $('.controls-state div').removeClass('active');
+                $('.controls-state-paused').addClass('active');
+                $(this).removeClass('playing').addClass('paused');
+            } else {
+                currentSound.play();
+                is_playing = true;
+                $('.controls-state div').removeClass('active');
+                $('.controls-state-playing').addClass('active');
+                $(this).removeClass('paused').addClass('playing');
+            }
+          } else {
+            SC.stream('/tracks/' + track_id).then(function(obj){
+                sound = obj;
+                obj.play();
+                is_playing = true;
+                $('.controls-state div').removeClass('active');
+                $('.controls-state-playing').addClass('active');
+                return false;
             });
-          });
-
+          }
           $track.removeClass('playing');
           $(this).addClass('playing');
-
-          closeTrackList();
-
-          $('.mbp-player-song-current-title').text(track_name);
+          
         });
-
       });
     });
 
