c2794f0c18eb51de63fdc130c49e663178ce2b85
Chris Porter <general@madebyporter.com>
2017-11-27 16:44:22 -0800

    music page design updates

--- source/assets/js/_global.js
+++ source/assets/js/_global.js
@@ -5,10 +5,11 @@ var js = js || {},
 
 js.main = {
   init: function () {
-    this.caseCarousel();
-    this.dribbble();
+    // this.caseCarousel();
+    // this.dribbble();
     this.fadeInScroll();
     this.linksExternal();
+    this.soundLibrary();
   },
 
   // Keep this shit in ABC Order
@@ -306,7 +307,124 @@ js.main = {
       window.open($(this).attr('href')); return false;
     });
   },
+  soundLibrary: function () {
+      // var dbx = new Dropbox({ accessToken: 'N-g23ovvhLQAAAAAAACYx1-nxB2mgRwZmNQ-nLLuouH4mtTlwzmZw9DSjES0ImmM' });
+      // dbx.filesListFolder({path: '/Music/mbp'})
+      //   .then(function(response) {
+      //     console.log(response);
+      //     var files = response.entries;
+      //     var list = document.getElementById('list');
+      //     var ol = document.createElement('ol');
+          
+      //     files.forEach( function(files) {
+      //       var li = document.createElement('li');
+      //       var div = document.createElement('div');
+      //       var a = document.createElement('a');
+      //       var song = files.name;
+      //       var date = files.client_modified;
+      //       div.innerHTML = song;
+      //       div.className += "sound-title";
+      //       li.appendChild(div);
+      //       li.dataset.date = date;
+      //       li.dataset.name = song;
+      //       li.className += "sound-set";
+      //       list.appendChild(li);
+      //     });
 
+      //     // Sorting
+      //     var $songs = $('.sound-list'),
+      //       $songsli = $songs.children('li');
+
+      //     $songsli.sort(function(a,b){
+      //       var an = a.getAttribute('data-date'),
+      //         bn = b.getAttribute('data-date');
+
+      //       if(an > bn) {
+      //         return -1;
+      //       }
+      //       if(an < bn) {
+      //         return 1;
+      //       }
+      //       return 0;
+      //     });
+
+      //     $songsli.detach().appendTo($songs);
+
+      //     // Remove MP3 tag
+      //     $('.sound-title').each(function() {
+      //       var $this = $(this);
+      //       $this.html($this.text().replace(/\b.mp3\b/g, ''));
+      //     });
+      //     mbpPlayer();
+      //   })
+      //   .catch(function(error) {
+      //     console.log(error);
+      //   });
+      function mbpPlayer(){
+        var audio;
+        var playlist;
+        var tracks;
+        var current;
+        var volume;
+        init();
+        function init(){
+          current = 0;
+          audio = $('audio');
+          playlist = $('.sound-list');
+          tracks = playlist.find('li');
+          len = tracks.length - 1;
+          audio[0].volume = 1;
+
+          play = $('#play');
+          pause = $('#pause');
+          mute = $('#mute');
+          muted = $('#muted');
+
+          playlist.find('.sound-title').click(function(e){
+              e.preventDefault();
+              link = $(this);
+              current = link.parent().index();
+              run(link, audio[0]);
+          });
+          audio[0].addEventListener('ended',function(e){
+              current++;
+              if(current == len){
+                  current = 0;
+                  // audio[0].pause();
+                  link = playlist.find('.sound-title')[0];
+              }else{
+                  link = playlist.find('.sound-title')[current];    
+              }
+              run($(link),audio[0]);
+          });
+        }
+        function run(link, player){
+          // var name = link.closest('.sound-set').attr('data-name');
+          var src = 'https://dl.dropboxusercontent.com/content_link/';
+          var title = link.html();
+          var url = src + title + '.mp3';
+          player.src = url;
+          // $('.mbp-player-current .title').html(name);
+          // $('.mbp-player-download').find('a').attr("href", url);
+          par = link.closest('.sound-set');
+          par.addClass('active').siblings().removeClass('active');
+          audio[0].load();
+          audio[0].play();
+
+          $('#play').replaceWith('<div id="pause" class="button-pause">Pause</div>');
+          $('.controls').addClass('active');
+        }
+        $(document).on('click', '#play', function(){
+          audio[0].play();
+          $(this).replaceWith('<div id="pause" class="button-pause">Pause</div>');
+        });
+
+        $(document).on('click', '#pause', function(){
+          audio[0].pause();
+          $(this).replaceWith('<div id="play" class="button-play">Play</div>');
+        });
+      }
+    },
 };
 
 doc.ready(function () {
