e9e1e890a4176ee5440710eb38673f7164e7111e
madebyporter <general@madebyporter.com>
2019-10-28 23:14:06 -0700

    updated journal and mission

--- source/assets/js/output.js
+++ source/assets/js/output.js
@@ -5,9 +5,10 @@ var js = js || {}, body = $('body'), doc = $(document), win = $(window);
 js.main = {
   init: function () {
     // this.lazyLoad();
-    // this.linksExternal();
+    this.linksExternal();
     // this.navWaypoints();
     this.mobileMenu();
+    // this.smoothScroll();
     // this.slick();
     // this.aos();
   },
@@ -124,6 +125,43 @@ js.main = {
     }
     createSlick();
     win.on('resize', createSlick);
+  },
+  smoothScroll: function() {
+    // Vanilla JavaScript Scroll to Anchor
+    // @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+
+    (function() {
+      scrollTo();
+    })();
+
+    function scrollTo() {
+      var links = document.getElementsByTagName('a');
+      for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
+          link.onclick = scrollAnchors;
+        }
+      }
+    }
+
+    function scrollAnchors(e, respond = null) {
+      const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+      e.preventDefault();
+      var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+      const targetAnchor = document.querySelector(targetID);
+      if (!targetAnchor) return;
+      const originalTop = distanceToTop(targetAnchor);
+      window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+      const checkIfDone = setInterval(function() {
+        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+        if (distanceToTop(targetAnchor) === 0 || atBottom) {
+          targetAnchor.tabIndex = '-1';
+          targetAnchor.focus();
+          window.history.pushState('', '', targetID);
+          clearInterval(checkIfDone);
+        }
+      }, 100);
+    }
   }
 };
 
