8f55dd05ff906af6a8b2678e383e918e120fc500
madebyporter <general@madebyporter.com>
2020-11-27 11:44:36 -0800

    fixed js issue

--- source/assets/js/_global.js
+++ source/assets/js/_global.js
@@ -4,94 +4,10 @@ js.main = {
   init: function () {
     this.linksExternal();
     this.mobileMenu();
-    this.clipboard();
-    this.prism();
     this.jsFeatures();
-    // this.waypointsNav();
   },
 
   // Keep this shit in ABC Order
-  clipboard: function () {
-    (function(){
-      if (typeof self === 'undefined' || !self.Prism || !self.document) {
-        return;
-      }
-    
-      if (!Prism.plugins.toolbar) {
-        console.warn('Copy to Clipboard plugin loaded before Toolbar plugin.');
-    
-        return;
-      }
-    
-      var ClipboardJS = window.ClipboardJS || undefined;
-    
-      if (!ClipboardJS && typeof require === 'function') {
-        ClipboardJS = require('clipboard');
-      }
-    
-      var callbacks = [];
-    
-      if (!ClipboardJS) {
-        var script = document.createElement('script');
-        var head = document.querySelector('head');
-    
-        script.onload = function() {
-          ClipboardJS = window.ClipboardJS;
-    
-          if (ClipboardJS) {
-            while (callbacks.length) {
-              callbacks.pop()();
-            }
-          }
-        };
-    
-        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js';
-        head.appendChild(script);
-      }
-    
-      Prism.plugins.toolbar.registerButton('copy-to-clipboard', function (env) {
-        var linkCopy = document.createElement('button');
-        linkCopy.textContent = 'Copy';
-        linkCopy.setAttribute('type', 'button');
-    
-        var element = env.element;
-    
-        if (!ClipboardJS) {
-          callbacks.push(registerClipboard);
-        } else {
-          registerClipboard();
-        }
-    
-        return linkCopy;
-    
-        function registerClipboard() {
-          var clip = new ClipboardJS(linkCopy, {
-            'text': function () {
-              return element.textContent;
-            }
-          });
-    
-          clip.on('success', function() {
-            linkCopy.classList.add('copied');
-            linkCopy.textContent = 'Copied!';
-            resetText();
-          });
-          clip.on('error', function () {
-            linkCopy.classList.add('copied');
-            linkCopy.textContent = 'Press Ctrl+C to copy';
-            resetText();
-          });
-        }
-    
-        function resetText() {
-          setTimeout(function () {
-            linkCopy.textContent = 'Copy';
-            linkCopy.classList.remove('copied');
-          }, 5000);
-        }
-      });
-    })();    
-  },
   jsFeatures: function () {
     var target = $('.js-features img');
     $('.js-features-list li').on('click', function(){
